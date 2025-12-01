e354c7b2962ceac62865ead37cef77a118b4ff29
florentlouman-lang <florent.louman@gmail.com>
2025-12-01 13:06:45 +0100

    Add event listener for worklet ready event

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
+// Attend que le worklet soit chargé
+window.addEventListener("worklet-ready", () => {
+    console.log("Worklet chargé 👍");
+    initPlayer();
+});
 console.log('placeholder app.js');
